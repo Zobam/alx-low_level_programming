fb7e2d059c083646f64c83590cacba44c1ade9cf
Chizoba <upc4you@gmail.com>
2022-04-07 15:13:39 -0700

    updated 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
 /**
+ * main - start point of the program
  *
- *main - start point of the program
- *
- *
- *
- *Return: (0) on success
- *
+ * Return: (0) on success
  */
 
 int main(void)
