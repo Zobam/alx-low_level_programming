deaddaeccc1e74aa301670a4d2e64a89372c12ed
Chizoba <upc4you@gmail.com>
2022-04-07 15:03:40 -0700

    updated 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,19 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *
- *  * main - start point of the program                                                                                                    
- *
- *   * Return: (0) on success                                                                                                               
- *
- *    */                                                                                                                                     
+#include <stdio.h>
 
-int main(void)                                                                                                                          
-{               
-		printf("with proper grammar, but the outcome is a piece of art,\n");	
-		return (0);                                                                                                                     
-} 
+/**
+ *
+ * *main - start point of the program
+ *
+ * *
+ *
+ * *Return: (0) on success
+ *
+ * */
+
+int main(void){
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
