91cb5fdc0c761c0a1b555cb38cc6b49a6606a04e
Chizoba <upc4you@gmail.com>
2022-04-08 12:26:25 -0700

    done 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start point of the program
+ *
+ * Return: (0) on success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		if(c != 'e' && c != 'q')
+			putchar(c);
+
+	putchar('\n');
+	return (0);
+}
