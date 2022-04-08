82e46fed054c78e7b26d63a3e18c27b27c02374e
Chizoba <upc4you@gmail.com>
2022-04-08 11:48:50 -0700

    done 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	if(last_digit > 5)
 		printf("greater than 5\n");
 	else if(last_digit == 0)
-		printf("zero\n");
+		printf("0\n");
 	else if(last_digit < 6 && last_digit != 0)
 		printf("less than 6 and not 0\n");
 	return (0);
