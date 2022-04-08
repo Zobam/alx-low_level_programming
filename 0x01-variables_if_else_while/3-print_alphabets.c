76050e80b560b72a29095144d85cdb1b035b9df9
Chizoba <upc4you@gmail.com>
2022-04-08 12:17:45 -0700

    done 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - start point of the program
+ *
+ * Return: (0) on success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; ++c)
+		putchar('a' + c);
+	for (c = 0; c < 26; ++c)
+		putchar('A' + c);
+
+	putchar('\n');
+	return (0);
+}
