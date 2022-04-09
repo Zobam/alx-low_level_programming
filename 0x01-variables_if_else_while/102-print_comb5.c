74692f63464ac4a3b62634db771a8fb4cbd212d5
Chizoba <upc4you@gmail.com>
2022-04-09 01:25:25 -0700

    done with 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - start point of the program
+ *
+ * Return: (0) on success
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 98; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y %/ 10) + '0');
+
+			if(x == 98 && y == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
