d217b1a34010fa0ef12dfece35854cf7679f275a
Chizoba <upc4you@gmail.com>
2022-04-12 13:23:39 -0700

    updated main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,7 +9,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_beuar(void);
+void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
