cb90fa2cf83a2f9fa55ab54d0fa693f84a3f85a6
Chizoba <upc4you@gmail.com>
2022-04-12 13:52:57 -0700

    done with 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @x: first operand
+ * @y: second operand
+ *
+ * Return: the sum of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
