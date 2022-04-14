fc842de6984a8ff39cf3e1680ed0385d50898f0d
Chizoba <upc4you@gmail.com>
2022-04-14 14:39:49 -0700

    done with 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,14 +3,14 @@
 /**
  * main -  test that the function positive_or_negative()
  * gives the correct output when given a case of 0
- * 
+ *
  * Return: returns 0
  */
 int main(void)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	positive_or_negative(i);
 	return (0);
 }
