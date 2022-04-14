03e949b2a7484fc3aaec60ab46f0d2aad78ffef7
Chizoba <upc4you@gmail.com>
2022-04-14 14:54:24 -0700

    done with 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - makes while loop run infinitely
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
