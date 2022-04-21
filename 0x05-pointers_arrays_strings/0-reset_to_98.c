5c4b9135f7a294254d1e31070a4af16437788ad6
Chizoba <upc4you@gmail.com>
2022-04-20 17:55:23 -0700

    done with 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of address to to 98
+ * @n: a pointer to argument address
+ * Return: zero always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README for pointer, array and string tasks in alx c programming course
