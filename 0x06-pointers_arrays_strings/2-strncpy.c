c7447207791e1c73d369959be475d026874ea740
Chizoba <upc4you@gmail.com>
2022-04-24 13:34:21 -0700

    done with 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: the number of bytes to copy from src
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (n > j)
+	{
+		if (src[j] == '\0')
+		{
+			for (; j < n; j++)
+			{
+				dest[i] = '\0';
+				i++;
+			}
+		}
+		else
+		{
+			dest[i] = src[j];
+			j++;
+			i++;
+		}
+	}
+	return (dest);
+}
