18f4db805cf77cab7446977b87c5750738006f4c
Chizoba <upc4you@gmail.com>
2022-05-02 08:35:56 -0700

    done with adding putchar lib

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - prints a character to output
+ * @c: character to be printed.
+ * Return: 0 on success and other number otherwise
+ */
+
+int _putchar(char c)
+{
+	return (0);
+}
