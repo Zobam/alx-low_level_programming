b090b6d51de84573257198c3a7ac77d6b1208a10
Chizoba <upc4you@gmail.com>
2022-05-13 22:00:19 -0700

    done with 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to be searched
+ * @size: size of array
+ * @cmp: pointer function
+ *
+ * Return: index of first found other number other wise
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+}
