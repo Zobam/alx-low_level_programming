33f94d31f6df4eb515467ed2d5dd082868dd981c
Chizoba <upc4you@gmail.com>
2022-05-14 20:09:48 -0700

    added stdion to function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FILE_F_POINTRS
 #define FILE_F_POINTRS
 
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
