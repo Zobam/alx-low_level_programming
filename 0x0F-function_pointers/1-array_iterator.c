8b0afd02710cf419b72b4f95dc33724dc2a6ed80
Chizoba <upc4you@gmail.com>
2022-05-14 20:21:23 -0700

    done with 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,22 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
+ * array_iterator - searches for an integer
  * @array: array to be searched
  * @size: size of array
- * @cmp: pointer function
+ * @action: pointer function
  *
  * Return: index of first found other number other wise
  */
 
-int int_index(int *array, int size, int (*cmp)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	int i;
+
+	if (array && size && action)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
 }
